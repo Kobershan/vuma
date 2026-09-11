6ef59497f5ba232cf14709ab44c4c9f864b8103e
kobershan <kobershan@gmail.com>
2026-09-11 04:11:43 +0000

    test: expand Stage 15 planning coverage

--- tests/VumaRetail.UnitTests/Planning/PlanningCoreTests.cs
+++ tests/VumaRetail.UnitTests/Planning/PlanningCoreTests.cs
@@ -49,6 +49,43 @@ public sealed class PlanningCoreTests
     }
 
     [Fact]
+    public void Forecast_strategies_honor_their_options_and_empty_series()
+    {
+        var options = new ForecastOptions(WindowSize: 2, Alpha: 0.5, SeasonLength: 2);
+
+        new MovingAverageForecastStrategy().Forecast([2m, 4m, 8m], options).Should().Be(6m);
+        new ExponentialSmoothingForecastStrategy().Forecast([2m, 4m, 8m], options).Should().Be(5.5m);
+        new SeasonalNaiveForecastStrategy().Forecast([2m, 4m, 8m], options).Should().Be(4m);
+        new MovingAverageForecastStrategy().Forecast([], options).Should().Be(0m);
+        new ExponentialSmoothingForecastStrategy().Forecast([], options).Should().Be(0m);
+        new SeasonalNaiveForecastStrategy().Forecast([], options).Should().Be(0m);
+    }
+
+    [Fact]
+    public void Forecast_rejects_invalid_alpha_and_clamps_moving_average_window()
+    {
+        var strategy = new ExponentialSmoothingForecastStrategy();
+        FluentActions.Invoking(() => strategy.Forecast([1m], new ForecastOptions(Alpha: 0)))
+            .Should().Throw<PlanningRuleException>();
+        FluentActions.Invoking(() => strategy.Forecast([1m], new ForecastOptions(Alpha: 1.1)))
+            .Should().Throw<PlanningRuleException>();
+
+        new MovingAverageForecastStrategy().Forecast([3m, 9m], new ForecastOptions(WindowSize: 99))
+            .Should().Be(6m);
+    }
+
+    [Fact]
+    public void Forecast_math_calculates_variation_and_standard_deviation_edge_cases()
+    {
+        ForecastMath.CoefficientOfVariation([10m, 10m, 10m]).Should().Be(0m);
+        ForecastMath.CoefficientOfVariation([0m, 0m]).Should().Be(0m);
+        ForecastMath.StandardDeviation([]).Should().Be(0m);
+        ForecastMath.StandardDeviation([5m]).Should().Be(0m);
+        ForecastMath.StandardDeviation([2m, 4m]).Should().BeApproximately(1.414213562m, 0.000000001m);
+        ForecastMath.ZScore(50m).Should().BeApproximately(0d, 0.000000001d);
+    }
+
+    [Fact]
     public void Safety_stock_uses_fallback_until_eight_weeks_and_rejects_short_horizon()
     {
         var calculator = new SafetyStockCalculator();
@@ -106,6 +143,24 @@ public sealed class PlanningCoreTests
     }
 
     [Fact]
+    public void Replenishment_uses_forecast_top_up_and_deterministic_transfer_tiebreaking()
+    {
+        Guid firstLocation = Guid.NewGuid();
+        Guid secondLocation = Guid.NewGuid();
+        var input = new ReplenishmentInput(
+            Guid.NewGuid(), null, Guid.NewGuid(), "EA", 10m, 2m, 12m, 0m, 20m, 5,
+            [new TransferCandidate(Guid.NewGuid(), firstLocation, 20m, At),
+             new TransferCandidate(Guid.NewGuid(), secondLocation, 20m, At)], false);
+
+        ProposedSuggestion result = new ReplenishmentEngine().Propose(input).Single();
+
+        result.Reason.Should().Be(SuggestionReason.TransferSurplus);
+        result.Source.Should().Be(SuggestionSource.Transfer);
+        result.Quantity.Should().Be(10m);
+        result.Candidate!.SourceLocationId.Should().Be(firstLocation.CompareTo(secondLocation) < 0 ? firstLocation : secondLocation);
+    }
+
+    [Fact]
     public void Replenishment_suggestion_is_exactly_once_and_preserves_recommendation_on_amendment()
     {
         var suggestion = ReplenishmentSuggestion.Raise(
@@ -139,5 +194,55 @@ public sealed class PlanningCoreTests
         plan.Status.Should().Be(MarkdownPlanStatus.Amended);
         amendment.Version.Should().Be(2);
         amendment.Status.Should().Be(MarkdownPlanStatus.Draft);
+    }
+
+    [Fact]
+    public void Markdown_planner_requires_all_three_inclusive_signals()
+    {
+        var planner = new MarkdownPlanner();
+        planner.Evaluate(new MarkdownSignals(AbcClass.C, XyzClass.Z, 20m, 90m))
+            .Should().Be(new MarkdownVerdict(true, 25m));
+        planner.Evaluate(new MarkdownSignals(AbcClass.B, XyzClass.Y, 20m, 90m))
+            .Should().Be(new MarkdownVerdict(false, 0m));
+        planner.Evaluate(new MarkdownSignals(null, XyzClass.Z, 21m, 120m))
+            .Should().Be(new MarkdownVerdict(false, 0m));
+    }
+
+    [Fact]
+    public void Markdown_plan_rejects_empty_submission_and_supports_cancel_and_complete()
+    {
+        var empty = MarkdownPlan.Create(Guid.NewGuid(), Guid.NewGuid(), "MDP-EMPTY", "reason",
+            new DateOnly(2026, 2, 1), null);
+        FluentActions.Invoking(() => empty.Submit(Guid.NewGuid())).Should().Throw<PlanningRuleException>();
+
+        var plan = MarkdownPlan.Create(Guid.NewGuid(), Guid.NewGuid(), "MDP-002", "reason",
+            new DateOnly(2026, 2, 1), null);
+        plan.AddLine(Guid.NewGuid(), null, 100m, 25m, "zar", "C-Z", 10m, 120m);
+        plan.Submit(Guid.NewGuid());
+        plan.ApplyApproval(true);
+        plan.RecordActivation(Guid.NewGuid());
+        plan.Complete();
+        plan.Status.Should().Be(MarkdownPlanStatus.Completed);
+        FluentActions.Invoking(plan.Cancel).Should().Throw<PlanningRuleException>();
+    }
+
+    [Fact]
+    public void Planning_domain_validates_sku_budget_and_date_invariants()
+    {
+        FluentActions.Invoking(() => DemandHistory.Create(Guid.NewGuid(), Guid.NewGuid(), Guid.NewGuid(),
+                null, null, new DateOnly(2026, 1, 5), new DateOnly(2026, 1, 11), 1m, "EA", At))
+            .Should().Throw<PlanningRuleException>();
+        FluentActions.Invoking(() => OpenToBuyBudget.Create(Guid.NewGuid(), Guid.NewGuid(), 2026, 13,
+                null, 100m, "zar")).Should().Throw<PlanningRuleException>();
+        FluentActions.Invoking(() => OpenToBuyBudget.Create(Guid.NewGuid(), Guid.NewGuid(), 2026, 1,
+                null, -1m, "zar")).Should().Throw<PlanningRuleException>();
+        FluentActions.Invoking(() => MarkdownPlan.Create(Guid.NewGuid(), Guid.NewGuid(), "MDP-003", "reason",
+                new DateOnly(2026, 2, 2), new DateOnly(2026, 2, 1))).Should().Throw<PlanningRuleException>();
+
+        var budget = OpenToBuyBudget.Create(Guid.NewGuid(), Guid.NewGuid(), 2026, 1, " cat ", 100m, " zar ");
+        budget.CategoryCode.Should().Be("cat");
+        budget.Currency.Should().Be("ZAR");
+        budget.Replan(75m);
+        budget.PlannedAmount.Should().Be(75m);
     }
 }
