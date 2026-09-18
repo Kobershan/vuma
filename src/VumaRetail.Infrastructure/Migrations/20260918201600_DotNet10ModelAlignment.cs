f4e5ffa12178d0249b425cbe88d59f11302dc3f1
kobershan <kobershan@gmail.com>
2026-09-18 20:19:57 +0000

    chore: align EF model snapshot with EF Core 10 [0.5]

---
+++ src/VumaRetail.Infrastructure/Migrations/20260918201600_DotNet10ModelAlignment.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VumaRetail.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class DotNet10ModelAlignment : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+                "ALTER TABLE finance.accounts RENAME CONSTRAINT pk_accounts TO pk_accounts1;");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+                "ALTER TABLE finance.accounts RENAME CONSTRAINT pk_accounts1 TO pk_accounts;");
+        }
+    }
+}
